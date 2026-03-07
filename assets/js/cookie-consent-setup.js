cdff8a2c32ab164f84016760e79163978fa99636
POSTECH-MinjaeLee <lalswo010@postech.ac.kr>
2026-03-07 09:43:01 +0000

    Deploying to gh-pages from @ POSTECH-MinjaeLee/lalswo010.github.io@387b55b36d89e46332c6910f93d21f4d6b0ca876 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://lalswo010.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://postech-minjaelee.github.io/lalswo010.github.io/#contact">contact us</a>.'
               }
             ]
           }
